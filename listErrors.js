87575e7fec7348d32c1e339ee15668b138bbca38
Cameron Parker <12camparker21@gmail.com>
2017-07-14 14:14:13 -0400

    Create URBAN Website Repo

---
+++ listErrors.js
@@ -0,0 +1,13 @@
+function listErrors (errors) 
+{
+  alert("Listing Errors");
+    console.log(errors);
+    var list = document.createElement('ul');
+    for (i = 0; i < errors.length; i++) 
+    {
+        var err = document.createElement('li');
+        err.appendChild(document.createTextNode(errors[i]));
+        list.appendChild(err);
+    }
+    return list;
+}
